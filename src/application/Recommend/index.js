262a5478fa8c56b07d61e625032eb8f11a4ccedf
liyaxuanliyaxuan <15196387158@163.com>
2020-07-03 19:39:03 +0800

    连接redux

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import { connect } from "react-redux";
 import * as actionTypes from './store/actionCreators';
 import Slider from "../../components/slider";
@@ -7,29 +7,59 @@ import { Content } from './style'
 
 import Scroll from '../../baseUI/scroll'
 function Recommend(props) {
-  const bannerList = [1, 2, 3, 4].map(item => {
-    return { imageUrl: 'http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg' }
-  })
+  // const bannerList = [1, 2, 3, 4].map(item => {
+  //   return { imageUrl: 'http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg' }
+  // })
 
-  const recommendList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(item => {
-    return {
-      id: 1,
-      picUrl: "https://p1.music.126.net/fhmefjUfMD-8qtj3JKeHbA==/18999560928537533.jpg",
-      playCount: 17171122,
-      name: "朴树、许巍、李健、郑钧、老狼、赵雷"
-    }
-  });
+  // const recommendList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(item => {
+  //   return {
+  //     id: 1,
+  //     picUrl: "https://p1.music.126.net/fhmefjUfMD-8qtj3JKeHbA==/18999560928537533.jpg",
+  //     playCount: 17171122,
+  //     name: "朴树、许巍、李健、郑钧、老狼、赵雷"
+  //   }
+  // });
+
+  const { bannerList, recommendList } = props;
+  const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
+
+  useEffect(() => {
+    getBannerDataDispatch();
+    getRecommendListDataDispatch();
+  }, [])
+
+  const bannerListJS = bannerList ? bannerList.toJS() : [];
+  const recommendListJS = recommendList ? recommendList.toJS() : [];
 
   return (
     <Content>
-      <Scroll className='list'>      
+      <Scroll className='list'>
         <div>
-          <Slider bannerList={bannerList}></Slider>
-          <RecommendList recommendList={recommendList}></RecommendList>
+          <Slider bannerList={bannerListJS}></Slider>
+          <RecommendList recommendList={recommendListJS}></RecommendList>
         </div>
       </Scroll>
     </Content>
   )
 }
 
-export default React.memo(Recommend);
+const mapStateToProps = (state) => ({
+  // 不要在这里将数据 toJS
+  // 不然每次 diff 比对 props 的时候都是不一样的引用，还是导致不必要的重渲染，属于滥用 immutable
+  bannerList: state.getIn (['recommend', 'bannerList']),
+  recommendList: state.getIn (['recommend', 'recommendList']),
+});
+// 映射 dispatch 到 props 上
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getBannerDataDispatch () {
+      dispatch (actionTypes.getBannerList ());
+    },
+    getRecommendListDataDispatch () {
+      dispatch (actionTypes.getRecommendList ());
+    },
+  }
+};
+
+// 将 ui 组件包装成容器组件
+export default connect (mapStateToProps, mapDispatchToProps)(React.memo (Recommend));
